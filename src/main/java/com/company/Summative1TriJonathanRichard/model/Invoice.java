97d56249b5d1b7f644a4efc60e63d871952f0c93
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-10-17 13:42:05 -0700

    initial upload

---
+++ src/main/java/com/company/Summative1TriJonathanRichard/model/Invoice.java
@@ -0,0 +1,4 @@
+package com.company.Summative1TriJonathanRichard.model;
+
+public class Invoice {
+}
---
+++ src/test/java/com/company/Summative1TriJonathanRichard/Summative1TriJonathanRichardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Summative1TriJonathanRichard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summative1TriJonathanRichardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
