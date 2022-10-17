807e9e42fda8811cca796c77d46cee5f8cddd506
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-10-17 14:11:16 -0700

    structure set up

---
+++ src/main/java/com/company/Summative1TriJonathanRichard/repository/TShirtRepository.java
@@ -0,0 +1,4 @@
+package com.company.Summative1TriJonathanRichard.repository;
+
+public interface TShirtRepository {
+}
---
+++ src/main/java/com/company/Summative1TriJonathanRichard/service/GameStoreServiceLayer.java
@@ -0,0 +1,4 @@
+package com.company.Summative1TriJonathanRichard.service;
+
+public class GameStoreServiceLayer {
+}
