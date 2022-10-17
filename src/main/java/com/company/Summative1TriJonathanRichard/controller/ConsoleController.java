ba2c680cc962791397d7aa735cd44880f95e4fd0
Tri Nguyen <tri.nguyen1086@gmail.com>
2022-10-17 14:17:03 -0700

    add CorsConfig

--- src/main/java/com/company/Summative1TriJonathanRichard/controller/ConsoleController.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/controller/ConsoleController.java
@@ -1,4 +1,9 @@
 package com.company.Summative1TriJonathanRichard.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/console")
 public class ConsoleController {
 }
