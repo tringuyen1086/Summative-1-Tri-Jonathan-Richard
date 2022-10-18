f4be854d02ffdf68cbf317cc2fbd1f377221a2e6
tringuyen1086 <tri.nguyen1086@gmail.com>
2022-10-18 12:59:24 -0700

    add @Repository repository Interfaces

--- src/main/java/com/company/Summative1TriJonathanRichard/repository/ProcessingFeeRepository.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/repository/ProcessingFeeRepository.java
@@ -1,4 +1,7 @@
 package com.company.Summative1TriJonathanRichard.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ProcessingFeeRepository {
 }
---
+++ src/test/java/com/company/Summative1TriJonathanRichard/controller/GameControllerTest.java
@@ -0,0 +1,7 @@
+package com.company.Summative1TriJonathanRichard.controller;
+
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+
+}
