fd21625db3aebc540c54bf703af1b524a9d810dc
Jonathan Tran <digitalsigna@gmail.com>
2022-10-18 08:18:32 -0700

    added console models and controllers to the project

---
+++ src/main/java/com/company/Summative1TriJonathanRichard/exceptions/NotFoundException.java
@@ -0,0 +1,6 @@
+package com.company.Summative1TriJonathanRichard.exceptions;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(){ super();}
+    public NotFoundException(String msg) { super(msg);}
+}
--- src/main/java/com/company/Summative1TriJonathanRichard/model/Console.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/model/Console.java
@@ -2,11 +2,121 @@ package com.company.Summative1TriJonathanRichard.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "console")
 public class Console {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int console_id;
+    @NotNull(message = "You must supply a value for model")
+    private String model;
+    @NotNull(message = "You must supply a value for manufacturer")
+    private String manufacturer;
+    private String memory_amount;
+    private String processor;
+    @NotNull(message = "You must supply a value for decimal")
+    private double decimal;
+    @NotNull(message = "You must supply a value for int")
+    private int quantity;
+
+    public Console(int console_id, String model, String manufacturer, String memory_amount, String processor, double decimal, int quantity) {
+        this.console_id = console_id;
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.memory_amount = memory_amount;
+        this.processor = processor;
+        this.decimal = decimal;
+        this.quantity = quantity;
+    }
+
+    public Console() {
+    }
+
+    public int getConsole_id() {
+        return console_id;
+    }
+
+    public void setConsole_id(int console_id) {
+        this.console_id = console_id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getMemory_amount() {
+        return memory_amount;
+    }
+
+    public void setMemory_amount(String memory_amount) {
+        this.memory_amount = memory_amount;
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public void setProcessor(String processor) {
+        this.processor = processor;
+    }
+
+    public double getDecimal() {
+        return decimal;
+    }
+
+    public void setDecimal(double decimal) {
+        this.decimal = decimal;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Console console = (Console) o;
+        return console_id == console.console_id && Double.compare(console.decimal, decimal) == 0 && quantity == console.quantity && Objects.equals(model, console.model) && Objects.equals(manufacturer, console.manufacturer) && Objects.equals(memory_amount, console.memory_amount) && Objects.equals(processor, console.processor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(console_id, model, manufacturer, memory_amount, processor, decimal, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Console{" +
+                "console_id=" + console_id +
+                ", model='" + model + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", memory_amount='" + memory_amount + '\'' +
+                ", processor='" + processor + '\'' +
+                ", decimal=" + decimal +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
