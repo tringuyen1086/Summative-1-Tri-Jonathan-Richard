e9ea65dcc2406b975c6fae2aeb60a46a8b5cf9ab
tringuyen1086 <tri.nguyen1086@gmail.com>
2022-10-18 12:43:50 -0700

    refactoring Class Name

--- src/main/java/com/company/Summative1TriJonathanRichard/repository/TShirtRepository.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/repository/TShirtRepository.java
@@ -2,9 +2,11 @@ package com.company.Summative1TriJonathanRichard.repository;
 
 import com.company.Summative1TriJonathanRichard.model.TShirt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TShirtRepository extends JpaRepository<TShirt,Integer> {
     public List<TShirt> findByColor(String color);
     public List<TShirt> findBySize(String size);
