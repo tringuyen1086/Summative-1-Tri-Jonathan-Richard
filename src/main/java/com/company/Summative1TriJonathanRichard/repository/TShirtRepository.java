53eceddae355a905a530cd3dde50240fee238aef
rich-f-p <richardferry3@gmail.com>
2022-10-18 13:49:16 -0400

    add code for t-shirt controller and functionality

--- src/main/java/com/company/Summative1TriJonathanRichard/repository/TShirtRepository.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/repository/TShirtRepository.java
@@ -1,4 +1,12 @@
 package com.company.Summative1TriJonathanRichard.repository;
 
-public interface TShirtRepository {
+import com.company.Summative1TriJonathanRichard.model.TShirt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TShirtRepository extends JpaRepository<TShirt,Integer> {
+    public List<TShirt> findByColor(String color);
+    public List<TShirt> findBySize(String size);
 }
+
