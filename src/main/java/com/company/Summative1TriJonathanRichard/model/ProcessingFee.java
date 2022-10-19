269ee67129b5e9fbd852f0dcdb9bd9ac764b224b
tringuyen1086 <tri.nguyen1086@gmail.com>
2022-10-18 17:09:05 -0700

    finish ProcessingFee class and SalesTaxRate class in model after adding Invoice class

--- src/main/java/com/company/Summative1TriJonathanRichard/model/ProcessingFee.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/model/ProcessingFee.java
@@ -2,12 +2,70 @@ package com.company.Summative1TriJonathanRichard.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "processing_fee")
 public class ProcessingFee {
 
+    @Id
+    @Column(name = "product_type")
+    @NotNull
+    @Size(max = 20)
+    private String productType;
+
+    private double fee;
+
+    public ProcessingFee(){
+
+    }
+
+    public ProcessingFee(String productType, double fee) {
+        this.productType = productType;
+        this.fee = fee;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProcessingFee that = (ProcessingFee) o;
+        return Double.compare(that.fee, fee) == 0 && Objects.equals(productType, that.productType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productType, fee);
+    }
+
+    @Override
+    public String toString() {
+        return "ProcessingFee{" +
+                "productType='" + productType + '\'' +
+                ", fee=" + fee +
+                '}';
+    }
 }
--- src/main/java/com/company/Summative1TriJonathanRichard/model/SalesTaxRate.java
+++ src/main/java/com/company/Summative1TriJonathanRichard/model/SalesTaxRate.java
@@ -3,10 +3,68 @@ package com.company.Summative1TriJonathanRichard.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Table(name = "sales_tax_rate")
 public class SalesTaxRate {
+
+    @Id
+    @NotNull
+    @Size(min = 2, max = 2)
+    private String state;
+
+    public SalesTaxRate(){
+
+    }
+
+    public SalesTaxRate(String state, double rate) {
+        this.state = state;
+        this.rate = rate;
+    }
+
+    @NotNull
+    private double rate;
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SalesTaxRate that = (SalesTaxRate) o;
+        return Double.compare(that.rate, rate) == 0 && Objects.equals(state, that.state);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(state, rate);
+    }
+
+    @Override
+    public String toString() {
+        return "SalesTaxRate{" +
+                "state='" + state + '\'' +
+                ", rate=" + rate +
+                '}';
+    }
 }
